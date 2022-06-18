8593797cd547463ca12311af780a08a134da53e8
DPatta <105843886+DPatta@users.noreply.github.com>
2022-06-18 22:08:54 +0700

    edit

--- db.js
+++ db.js
@@ -1,11 +1,22 @@
-const mysql = require("mysql2");
-const dbConnection = mysql.createPool({
-    host: "arjuna.db.elephantsql.com",
-    user: "vzxibjab",
-    password: "2B9TAgNT-BnF-sxKBGe-EXycTPe93IGD",
-    database: "vzxibjab"
+// const mysql = require("mysql2");
+// const dbConnection = mysql.createPool({
+//     host: "arjuna.db.elephantsql.com",
+//     user: "vzxibjab",
+//     password: "2B9TAgNT-BnF-sxKBGe-EXycTPe93IGD",
+//     database: "vzxibjab"
 
-}).promise()
+// }).promise()
 
-module.exports = dbConnection;
+// module.exports = dbConnection;
+let { Client } = require("pg");
+const dbConfig = {
+  connectionString:
+    "postgres://vzxibjab:2B9TAgNT-BnF-sxKBGe-EXycTPe93IGD@arjuna.db.elephantsql.com/vzxibjab",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
+const conn = new Client(dbConfig);
+conn.connect();
+module.exports = conn;
