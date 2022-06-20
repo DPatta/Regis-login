7622415c208d30a95b2453f35b8108b3a5d8838f
DPatta <105843886+DPatta@users.noreply.github.com>
2022-06-20 16:20:33 +0700

    edit

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-const pg = require("pg");
-const dbConnection = pg.createPool({
+const mysql = require("mysql2");
+const dbConnection = mysql.createPool({
     host: "arjuna.db.elephantsql.com",
     user: "vzxibjab",
     password: "2B9TAgNT-BnF-sxKBGe-EXycTPe93IGD",
